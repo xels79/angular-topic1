29150ae9b117e1f46d416ce8d846e4977b987a69
Alexander <xel_s@mail.ru>
2024-05-05 23:18:13 +0300

    tws 0.1

--- src/app/services/interceptors/rest-iterceptors.service.ts
+++ src/app/services/interceptors/rest-iterceptors.service.ts
@@ -11,10 +11,12 @@ export class RestIterceptorsService implements HttpInterceptor {
   constructor(private userService:UserService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.userService.getToken();
+    //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3QiLCJpYXQiOjE3MTQ5MzY0MzgsImV4cCI6MTcxNDkzNzAzOH0.exgip43w1jXsR_s_6y4OHdvNiRXVeLiS9LWbSo9TY6g";
     if (token){
-      return next.handle(req.clone({
+      const newReq = next.handle(req.clone({
         headers: req.headers.set("Autorization", `Bearer ${token}`)
-      }))
+      }));
+      return newReq;
     }else{
       return next.handle(req);
     }
